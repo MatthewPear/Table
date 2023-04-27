f8b56b9146076714b381ef51ad2ef66456be3278
MatthewPear <matthewpear@hotmail.com>
2023-04-27 17:57:02 +0200

    mk5

--- main.js
+++ main.js
@@ -6,13 +6,13 @@ document.body.appendChild(myTable);
 const tblBody = document.createElement("tbody");
 myTable.appendChild(tblBody);
 
-for (let i = 0; i < 10; i++) {
+for (let i = 1; i < 11; i++) {
     const myRow = document.createElement("TR");
     myRow.setAttribute("id", "row" + i);
     myRow.classList.add("row");
     tblBody.appendChild(myRow);
 
-    for (let j = 0; j < 10; j++) {
+    for (let j = 1; j < 11; j++) {
         const myData = document.createElement("TD");
         myData.setAttribute("id", "data" + j);
         myData.classList.add("data");
@@ -20,4 +20,15 @@ for (let i = 0; i < 10; i++) {
     }
 }
 
+const inputRow = document.getElementById('rowInput');
+const inputData = document.getElementById('dataInput');
+const btnFind = document.getElementById('button');
+btnFind.addEventListener('click', () => findCell());
 
+function findCell () {
+    console.log("row:", inputRow.value);
+    console.log("data:", inputData.value);
+
+    // myData.classList.add("data red");
+
+}
