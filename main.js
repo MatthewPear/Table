b42ea995681ec41270b7fa62eedea05f16783a1c
MatthewPear <matthewpear@hotmail.com>
2023-04-27 15:26:51 +0200

    mk1

---
+++ main.js
@@ -0,0 +1,23 @@
+const myTable = document.createElement("TABLE");
+myTable.setAttribute("id", "myTable");
+myTable.classList.add("table");
+document.body.appendChild(myTable);
+
+const tblBody = document.createElement("tbody");
+myTable.appendChild(tblBody);
+
+for (let i = 0; i < 10; i++) {
+    const myRow = document.createElement("TR");
+    myRow.setAttribute("id", "myRow");
+    myRow.classList.add("tr");
+    tblBody.appendChild(myRow);
+
+    for (let i = 0; i < 10; i++) {
+        const myData = document.createElement("TD");
+        myData.setAttribute("id", "myData");
+        myData.classList.add("td");
+        myRow.appendChild(myData);
+    }
+}
+
+
